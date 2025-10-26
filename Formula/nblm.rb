359842fecc73eb8bf8cc3c8c2020f97ec302565f
K <51281148+K-dash@users.noreply.github.com>
2025-10-26 20:38:50 +0900

    first commit

---
+++ Formula/nblm.rb
@@ -0,0 +1,26 @@
+# Formula/nblm.rb
+class Nblm < Formula
+    desc "NotebookLM Enterprise CLI"
+    homepage "https://github.com/K-dash/nblm-rs"
+    license "MIT"
+    version "0.1.5"
+
+    on_macos do
+      on_arm do
+        url "https://github.com/K-dash/nblm-rs/releases/download/v0.1.5/nblm-macos-aarch64.tar.gz"
+        sha256 "4c68904c08f18a39e86c4a04231e23c85f35f3a94314e7fb8e00ead57d553888"
+      end
+      on_intel do
+        url "https://github.com/K-dash/nblm-rs/releases/download/v0.1.5/nblm-macos-x86_64.tar.gz"
+        sha256 "d165559cd41bc71b456af9fa30c2083383ea949bb2a6f6131519109183a85c7a"
+      end
+    end
+
+    def install
+      bin.install "nblm"
+    end
+
+    test do
+      system "#{bin}/nblm", "--version"
+    end
+  end
