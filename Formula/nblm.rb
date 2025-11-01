3a3283fd2d153e1d570e6e011385a3de4a96a0c3
𝕂 <51281148+K-dash@users.noreply.github.com>
2025-11-01 17:08:51 +0900

    Bump Nblm version to 0.2.0

--- Formula/nblm.rb
+++ Formula/nblm.rb
@@ -3,15 +3,15 @@ class Nblm < Formula
     desc "NotebookLM Enterprise CLI"
     homepage "https://github.com/K-dash/nblm-rs"
     license "MIT"
-    version "0.1.5"
+    version "0.2.0"
 
     on_macos do
       on_arm do
-        url "https://github.com/K-dash/nblm-rs/releases/download/v0.1.5/nblm-macos-aarch64.tar.gz"
+        url "https://github.com/K-dash/nblm-rs/releases/download/v0.2.0/nblm-macos-aarch64.tar.gz"
         sha256 "4c68904c08f18a39e86c4a04231e23c85f35f3a94314e7fb8e00ead57d553888"
       end
       on_intel do
-        url "https://github.com/K-dash/nblm-rs/releases/download/v0.1.5/nblm-macos-x86_64.tar.gz"
+        url "https://github.com/K-dash/nblm-rs/releases/download/v0.2.0/nblm-macos-x86_64.tar.gz"
         sha256 "d165559cd41bc71b456af9fa30c2083383ea949bb2a6f6131519109183a85c7a"
       end
     end
